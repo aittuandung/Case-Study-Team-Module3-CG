7c5604f02f3ed63ffd3e8f5a737db6ede322518a
aittuandung <tuandung.ait.it@gmail.com>
2022-07-06 09:03:36 +0700

    upp model and connect_mysl

---
+++ src/main/java/model/Apartment.java
@@ -0,0 +1,92 @@
+package model;
+
+import java.util.Date;
+
+public class Apartment {
+    int idCH;
+    String address;
+    double price;
+    double area;
+    String picture;
+    String status;
+    String description;
+    Date datePost;
+
+    public Apartment() {
+    }
+
+    public Apartment(int idCH, String address, double price, double area, String picture, String status, String description, Date datePost) {
+        this.idCH = idCH;
+        this.address = address;
+        this.price = price;
+        this.area = area;
+        this.picture = picture;
+        this.status = status;
+        this.description = description;
+        this.datePost = datePost;
+    }
+
+    public int getIdCH() {
+        return idCH;
+    }
+
+    public void setIdCH(int idCH) {
+        this.idCH = idCH;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public void setArea(double area) {
+        this.area = area;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDatePost() {
+        return datePost;
+    }
+
+    public void setDatePost(Date datePost) {
+        this.datePost = datePost;
+    }
+}
--- src/main/java/model/Customer.java
+++ src/main/java/model/Customer.java
@@ -3,5 +3,78 @@ package model;
 public class Customer {
     int idKH ;
     String fullName;
+    String birthDay;
+    String idCart;
+    String homeTown;
+    String phoneNumber;
+    double wallet;
 
+    public Customer() {
+    }
+
+    public Customer(int idKH, String fullName, String birthDay, String idCart, String homeTown, String phoneNumber, double wallet) {
+        this.idKH = idKH;
+        this.fullName = fullName;
+        this.birthDay = birthDay;
+        this.idCart = idCart;
+        this.homeTown = homeTown;
+        this.phoneNumber = phoneNumber;
+        this.wallet = wallet;
+    }
+
+    public int getIdKH() {
+        return idKH;
+    }
+
+    public void setIdKH(int idKH) {
+        this.idKH = idKH;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(String birthDay) {
+        this.birthDay = birthDay;
+    }
+
+    public String getIdCart() {
+        return idCart;
+    }
+
+    public void setIdCart(String idCart) {
+        this.idCart = idCart;
+    }
+
+    public String getHomeTown() {
+        return homeTown;
+    }
+
+    public void setHomeTown(String homeTown) {
+        this.homeTown = homeTown;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public double getWallet() {
+        return wallet;
+    }
+
+    public void setWallet(double wallet) {
+        this.wallet = wallet;
+    }
 }
